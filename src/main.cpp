5bf2164df1ffed15eaa6052c75f9fb962002de07
PasztorGergo <pasztorg05@gmail.com>
2025-12-19 12:03:44 +0100

    OTA communication

--- src/main.cpp
+++ src/main.cpp
@@ -1,50 +1,57 @@
 #include <Arduino.h>
 #include <WiFi.h>
-#include "MQTT_Cyan.h"
 #include "Secrets.h"
+#include <ArduinoOTA.h>
 
-void connect_to_wifi(const char *ssid, const char *password);
+bool connect_to_wifi(const char *ssid, const char *password);
 
 #define INPUT_PIN 4
 #define MQTT_TOPIC "esp32/topic"
 
-MQTTCyan *client1;
 const char *dummy_msg =  "Hi from silly esp32 :3";
+esp_mqtt_client_handle_t handler = esp_mqtt_client_init(&mqtt_config);
 
 void setup() {
     delay(100);
     Serial.begin(115200);
 
-    connect_to_wifi(project_ssid, passwd);
+    const bool is_connected = connect_to_wifi(project_ssid, passwd);
+
+    Serial.print(is_connected);
+
+    if(is_connected){
+        Serial.println("Connected to WiFi");
+        esp_mqtt_client_start(handler);
+        ArduinoOTA.begin();
+    }
 
     //Setting up input pin for sensing
     pinMode(INPUT_PIN, INPUT);
 }
 
 void loop() {
+    ArduinoOTA.handle();
+
     if(digitalRead(INPUT_PIN) == HIGH){
-        client1->publish(MQTT_TOPIC, dummy_msg, 1, 0);
+        esp_mqtt_client_publish(handler, MQTT_TOPIC, dummy_msg, 0, 1, 0);
         delay(100);
     }
-    Serial.println("Paws at you, hi :3");
+    //Serial.println("Paws at you, hi :3");
     delay(5000);
 
 }
 
-void connect_to_wifi(const char *ssid, const char *password){
+bool connect_to_wifi(const char *ssid, const char *password){
     WiFi.begin(ssid, password);
     uint8_t retries = 0;
 
     for(uint8_t i = 0; i < 10U; i++){
         if(WiFi.status() == WL_CONNECTED)
-            break;
+            return true;
         
         Serial.print(".");
         delay(500);
     }
 
-    Serial.println("");
-    Serial.printf("Successfully connected to %s\n", ssid);
-    Serial.printf("IP address: %s", WiFi.localIP());
-    client1 = new MQTTCyan(mqtt_config);
+    return false;
 }
